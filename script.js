09865b7e2033cf266d9afd1c0bf98f1edca4329d
alex <alexmandrila@gmail.com>
2019-01-28 17:16:50 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-// JS for Colorado Trail website
+var departDate = "2018-06-15";
 
-var timeKeep = "Time Left";
+function getTimeRemaining(e) {
+	var t = Date.parse(e) - Date.parse(new Date());
+	var days = Math.floor( t/(1000*60*60*24) );
+	var hours = Math.floor( (t/(1000*60*60))%24 );
+	var mins = Math.floor( (t/(1000*60))%60 );
+	var secs = Math.floor( (t/(1000))%60 );
+	return {
+		'total': t,
+		'days': days,
+		'hours': hours,
+		'mins': mins,
+		'secs': secs,
+		'print': function printTime(val) {
+			var timeString = "<ul><li>" + val.days + " days</li>";
+			timeString += "<li>" + val.hours + " hours</li>";
+			timeString += "<li>" + val.mins + " mins</li>";
+			timeString += "<li>" + val.secs + " secs</li></ul>";
+			document.getElementById("ct-timer").innerHTML = timeString;
+		}
+	}
+}
 
-console.log(document.getElementById("time-keep").textContent);
+// function initializeClock(id, endtime) {
+// 	var clock = document.getElementById(id);
+// 	var timerInterval = setInterval(function() {
+		
+// 	});
+// }
 
-document.getElementById("time-keep").textContent = timeKeep;
+// document.getElementById("ct-timer").innerHTML = "changed!";
+
+var run = function() {
+	var timeLeft = getTimeRemaining(departDate);
+	timeLeft.print(timeLeft);
+	// document.getElementById("ct-timer").innerHTML = daysLeft.total;
+	// return daysLeft;
+}
+
+window.onload = function() { //run the code after the DOM is constructed, resulting in the document element "ct-timer" having a value
+	setInterval(run, 100);	
+}
+
